27646ce9b527dfbe377874dd422d86176a6eeb37
YangHui <2411046022@qq.com>
2020-03-14 16:28:49 +0800

    服务器测试代码

---
+++ NetTestS.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class NetTestS {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ServerSocket ss = new ServerSocket(7000);
+		final Socket s = ss.accept();
+		System.out.println("Connect success");
+		
+		
+		class PutThread extends Thread{
+			@Override
+			public void run(){
+				Scanner sc = new Scanner(System.in);
+				OutputStreamWriter osw;
+				try {
+					osw = new OutputStreamWriter(s.getOutputStream());
+					while (true) {
+						osw.write(sc.nextLine() + "\n");
+						osw.flush();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		PutThread t1 = new PutThread();
+		t1.start();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		while (true) {
+			System.out.println(">" + br.readLine());
+			
+		}
+	}
+}
