e58f4e3f7f0de3778b3a9783fc93302357bb5b37
YangHui <2411046022@qq.com>
2020-03-17 08:27:11 +0800

    完善了服务器代码，解决了资源泄露的问题

--- NetTestS.java
+++ NetTestS.java
@@ -8,37 +8,48 @@ import java.util.Scanner;
 
 public class NetTestS {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		ServerSocket ss = new ServerSocket(7000);
-		final Socket s = ss.accept();
-		ss.close();
-		System.out.println("Connect success");
-		
-		
-		class PutThread extends Thread{
-			@Override
-			public void run(){
-				Scanner sc = new Scanner(System.in);
-				OutputStreamWriter osw;
-				try{
-					osw = new OutputStreamWriter(s.getOutputStream());
-					while (true) {
-						osw.write(sc.nextLine() + "\n");
-						osw.flush();
+	public static void main(String[] args) {
+		try {
+			ServerSocket ss = new ServerSocket(7000);
+			final Socket s = ss.accept();
+			ss.close();
+			System.out.println("Connect success");
+
+			class PutThread extends Thread {
+				@Override
+				public void run() {
+					Scanner sc = new Scanner(System.in);
+					try {
+						OutputStreamWriter osw = new OutputStreamWriter(s.getOutputStream());
+						String tmpString;
+						while (true) {
+							tmpString = sc.nextLine();
+							if(tmpString.equals("end")) {
+								break;
+							}
+							osw.write(tmpString + "\n");
+							osw.flush();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}finally{
+						sc.close();
 					}
-				} 
-				catch (IOException e){
-					e.printStackTrace();
 				}
-				sc.close();
 			}
-		}
-		PutThread t1 = new PutThread();
-		t1.start();
-		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		while (true) 
-		{
-			System.out.println(">" + br.readLine());	
+			new PutThread().start();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String tmpString;
+			while (true) {
+				tmpString = br.readLine();
+				if(tmpString == null) {
+					break;
+				}
+				System.out.println(">" + tmpString);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
